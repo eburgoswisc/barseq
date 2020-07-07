7fd9941276657a0b50568a474d49a025656faab1
Emanuel Burgos <eburgos@wisc.edu>
2020-07-07 16:04:11 -0500

    Created a Barseq runner class

--- barseq/main.py
+++ barseq/main.py
@@ -17,79 +17,91 @@ from barseq.utils import *
 __author__ = 'Emanuel Burgos'
 __email__ = 'eburgos@wisc.edu'
 
-
-__author__ = "Emanuel Burgos"
-__email__ = "eburgos@wisc.edu"
-
 # Get logger
-logger = logging.getLogger("barseq")
+logger = logging.getLogger()
+
+class BarSeqRun():
+    def __init__(self, input: str, barcodes: str, output_dir: str, experiment: str, sample_map: str=None, force_it: bool=False):
+
+        self.program = 'BarSeq'
+        self.experiment = experiment
+        self.sequence_dir = Path(input)
+        self.barcodes_path = Path(barcodes)
+        self.output_dir = Path(output_dir).joinpath(f'results/{self.experiment}')
+        self.logfile = self.output_dir.joinpath(f'{self.experiment}.log')
+        self.counts_results_path = self.output_dir.joinpath('barcode_counts_table.csv')
+
+        # Create the output directory
+        create_directories(self.output_dir, force_it)
+
+        # Add logfile handler to logger
+        #BarSeqLogger.add_file_handler(self.logfile)
+
+        if sample_map:
+            self.sample_map_path = Path(sample_map)
 
 
-class Run:
-    """ Class that stores settings for barseq processes. """
-    def __init__(self, args):
-        self.experiment = args.experiment
-        self.sequences = Path(args.input)
-        self.barcodes = Path(args.barcodes)
-        # self.barseq_sample_collection = list()
-        self.sample_dict = dict()
-        self.path = f"results/{self.experiment}/"
-        self.log = f"{self.path}log.txt"
 
+    def get_sequence_files(self):
+        """ Gets sequence files relative path and returns list with Path objects
+        :return: list with relative path objects from cwd for sequences
+        """
+        # Relative to cwd Path objects (cwd/sequences/filename.fastq[.gz])
+        return sorted([Path(n) for n in self.sequence_dir.glob('*fastq*')])
 
-class SampleRecord:
-    """ Class for storing sample properties. """
-    def __init__(self, sample: str, filename: str, barcode_dict):
-        self.sample = sample
-        self.filename = filename
-        self.barcode_dict = deepcopy(barcode_dict)
-        self.LEFT_SEQUENCE = ""
-        self.RIGHT_SEQUENCE = ""
+    def rename_sample_columns(self):
+        new_barcode_count = dict()
+        for sample, d in self.sample_map.items():
+            if sample in self.barcode_count_samples_dict.keys():
+                new_barcode_count[(d['Name'], d['Type'])] = self.barcode_count_samples_dict[sample]
+        self.barcode_count_samples_dict = new_barcode_count
+        return
 
+    def run(self) -> None:
+        """
+        Main pipeline for analyzing barseq data.
+        """
+        logger.info("***** Starting barseq *****")
 
-def main(args) -> None:
-    """
-    Main pipeline for analyzing barseq data. Will be changed to be more modular, for now
-    I just need the algorithm worked out.
+        logger.info(f"Collecting sequence files from {self.sequence_dir}")
+        self.sequences_list = self.get_sequence_files()
 
-    """
-    # ---- SET UP SETTINGS ---- #
-    runner = Run(args)
-    make_barseq_directories(runner)
-    # Add file handler
-    fh = logging.FileHandler(runner.log, mode="w")
-    fh.setFormatter(logging.Formatter(
-        "%(asctime)s - %(levelname)s - %(module)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M"))
-    logger.addHandler(fh)
-    logger.info("***** Starting barseq *****")
-    # Read in barcode
-    logger.info(f"Reading in barcodes from {runner.barcodes.name}")
-    barcodes = read_barcodes(runner.barcodes)
-    # Process each sequencing file
-    seq_files_list = sorted(os.listdir(runner.sequences))
-    for seq_file in seq_files_list:
-        if not seq_file.endswith(".DS_Store"):
-            sample = format_filename(seq_file)
-            logger.info(f"Counting Barcodes in {sample}")
-            runner.sample_dict[sample] = deepcopy(barcodes)
-            # Change cwd
-            with Cd(runner.sequences):
-                count_barcodes(seq_file, runner.sample_dict[sample])
+        logger.info(f"Reading in barcodes from {self.barcodes_path.name}")
+        self.barcode_dict = read_barcodes(self.barcodes_path)
+        self.barcode_count_samples_dict = dict()
 
-    # TODO: Add basic analysis
+        if self.sample_map_path:
+            self.sample_map = read_sample_map(self.sample_map_path)
 
-    # Write to output
-    logger.info(f"Writing results to {runner.path}")
-    write_output(runner.sample_dict, barcodes, runner)
+        # Process each sequencing file
+        for seq_file in self.sequences_list:
+            # Get sample name without suffixes
+            suffixes = ''.join(seq_file.suffixes)
+            sample = seq_file.name.replace(suffixes, '')
 
-    # Confirm completion of barseq
-    logger.info("***** barseq is complete! *****")
+            if 'R2' in seq_file.name.split('_'):
+                logger.warning(f'Skipping {sample} since software does not support paired-end reads yet.')
+                continue
+
+            logger.info(f"Mapping barcoded reads in {sample}")
+            self.barcode_count_samples_dict[sample] = deepcopy(self.barcode_dict)
+            # Seq file needs to be just name
+            count_barcodes(seq_file, self.barcode_count_samples_dict[sample])
+
+        # Change names
+        if self.sample_map_path:
+            logger.info(f"Renaming columns using sample mapping found in {self.sample_map_path.name}")
+            self.rename_sample_columns()
+
+        # Write to output
+        logger.info(f"Storing results in {self.output_dir}")
+        logger.info(f"Writing count data into {self.counts_results_path}")
+        write_output(self.barcode_count_samples_dict, self.barcode_dict, self.counts_results_path)
+
+        # Confirm completion of barseq
+        logger.info("***** barseq is complete! *****")
+        return
 
 
 if __name__ == "__main__":
-
-    """ # -------- START HERE --------  # """
-    args = sys.argv[1:]
-    main(args)
-
+    pass
