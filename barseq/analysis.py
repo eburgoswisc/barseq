18d189188629670918bbb1c77e8cf8bcc8cef1bc
Emanuel Burgos <eburgos@wisc.edu>
2020-07-08 12:14:08 -0500

    Analysis pipeline is now functional. Normalizes samples to input and returns a competitive index table

--- barseq/analysis.py
+++ barseq/analysis.py
@@ -12,7 +12,7 @@ from pathlib import Path
 
 # Module imports
 from barseq.read_barcodes import read_barcodes
-from barseq.utils import read_sample_map
+from barseq.utils import *
 
 __author__ = 'Emanuel Burgos'
 __email__ = 'eburgos@wisc.edu'
@@ -22,8 +22,7 @@ logger = BarSeqLogger.logger
 
 
 class BarSeqAnalysis(object):
-    def __init__(self, cts_table: str, sample_map: str, barcodes: str, output_dir: str, rename_sample=False,
-                 read_bar=False):
+    def __init__(self, cts_table: str, sample_map: str, barcodes: str, output_dir: str):
 
         self.count_file = Path(cts_table)
         self.output_dir = Path(output_dir)
@@ -33,15 +32,8 @@ class BarSeqAnalysis(object):
         self.raw_counts_df.drop(labels=['_other'], inplace=True)
         self.raw_counts_df.drop(columns=['Barcodes'], inplace=True)
 
-        if read_bar:
-            self.barcode_map = read_barcodes(barcodes)
-        else:
-            self.barcode_map = barcodes
-
-        if rename_sample:
-            self.sample_map = read_sample_map(sample_map)
-        else:
-            self.sample_map = sample_map
+        self.barcode_map = barcodes
+        self.sample_map = sample_map
 
         # Find identifiers
         self.n_samples = len(self.raw_counts_df.columns)
@@ -62,39 +54,27 @@ class BarSeqAnalysis(object):
         # Mapping tables
         self.sum_sample_strains = self.raw_counts_df.sum(axis=0)
 
-        self.get_rf_input()
-
+        logger.info("Normalize raw counts to input barcode counts.")
         self.normalize_counts()
 
+        logger.info("Calculate relative frequency for each barcode.")
         self.calculate_rf()
 
-        self.average_wt()
-        self.get_ci_values()
+        logger.info("Calculate the competitive index.")
+        self.calculate_competitive_index()
 
         # Save final df into results file
         self.final_df.to_csv(self.output_dir.joinpath('barcode-counts-analysis.txt'), sep='\t')
-        return self.final_df
 
-    def calculate_rf(self):
-        # Loop through each col, get relative frequency
-        self.rf_normalized_output = pd.DataFrame(index=self.raw_counts_df.index)
-        for col in self.normalized_df:
-            # Get sum of all counts in sample
-            sum_counts_sample = self.normalized_df[col].sum()
-            rf_sample = dict()
-            for strain, count in self.normalized_df[col].iteritems():
-                rf_sample[strain] = count / sum_counts_sample
-            self.rf_normalized_output = self.rf_normalized_output.join(pd.Series(rf_sample, name=col))
-
-    def get_rf_input(self):
-        self.rf_input = dict()
-        for strain, counts in self.raw_counts_df[self.input_id].iteritems():
-            self.rf_input[strain] = counts / self.sum_sample_strains[self.input_id]
         return
 
     def normalize_counts(self):
+        self.rf_input = dict()
+        for strain, counts in self.raw_counts_df[self.input_id].iteritems():
+            self.rf_input[strain] = counts / self.sum_sample_strains[self.input_id]
+
         # Loop through each sample column, return series with strain as index
-        self.normalized_df = pd.DataFrame(index=self.raw_counts_df.index)
+        self.normalized_counts = pd.DataFrame(index=self.raw_counts_df.index)
         for col in self.raw_counts_df:
             normalized_sample = dict()
             # Skip gene column
@@ -102,15 +82,24 @@ class BarSeqAnalysis(object):
                 for strain, count in self.raw_counts_df[col].iteritems():
                     # Place in dictionary
                     normalized_sample[strain] = (count / self.rf_input[strain]) / self.n_samples
-                self.normalized_df = self.normalized_df.join(pd.Series(normalized_sample,
-                                                                       index=self.raw_counts_df.index, name=col))
+                self.normalized_counts = self.normalized_counts.join(pd.Series(normalized_sample,
+                                                                               index=self.raw_counts_df.index, name=col))
 
-    def average_wt(self):
+    def calculate_rf(self):
+        # Loop through each col, get relative frequency
+        self.rf_normalized_output = pd.DataFrame(index=self.raw_counts_df.index)
+        for col in self.normalized_counts:
+            # Get sum of all counts in sample
+            sum_counts_sample = self.normalized_counts[col].sum()
+            rf_sample = dict()
+            for strain, count in self.normalized_counts[col].iteritems():
+                rf_sample[strain] = count / sum_counts_sample
+            self.rf_normalized_output = self.rf_normalized_output.join(pd.Series(rf_sample, name=col))
+
+    def calculate_competitive_index(self):
         # Grab only WT rows and take mean
         rf_norm_out_mean_WT = self.rf_normalized_output.loc[self.WT_id, :].mean()
         self.rf_norm_mean_WT = {col: wt_rf for col, wt_rf in rf_norm_out_mean_WT.iteritems()}
-
-    def get_ci_values(self):
         self.final_df = pd.DataFrame(index=self.rf_normalized_output.index)
         wt_input = self.rf_norm_mean_WT[self.input_id]
         for sample in self.rf_normalized_output:
